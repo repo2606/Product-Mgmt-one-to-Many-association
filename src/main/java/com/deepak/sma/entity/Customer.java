bc8623c4e1b05cfe1be9ab7b45e8a4fca3fa821a
Deepak <130002300+repo2606@users.noreply.github.com>
2023-05-05 18:10:34 -0400

    Update Customer.java

--- src/main/java/com/deepak/sma/entity/Customer.java
+++ src/main/java/com/deepak/sma/entity/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
 	private String email;
 	
 	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.PERSIST},
-			fetch = FetchType.LAZY) // e LAZYager//
+			fetch = FetchType.LAZY) 
 	@JoinColumn(name="product_id")    
 	private Product theproducts;
 	
@@ -93,34 +93,3 @@ public class Customer {
 }
 
 
-/*   
- * 
- * Theory
- * 
- * ManyCustomer mapped to single product
- * customer -Many  -owning side
- * 
- * Products - one _ Inverse side
- * 
- * owning side (Customer) should have -Join columns specified ie foreign key which connects to product table
- * 
- * Inverse side(products)- should have mappedby (which includes the field that has annotated with JoinColumn
- * otherwise mappedBy should contain field of owning class(Customer) which has foreign key "product_id "that connects the 
- * product table
- * 
- * Cascade - whatever changes applied to parent associated child will undergo those changes
- * REMOVE: when parent (product) is removed , its child(employees associated with product is also removed)
- * PERSIST:when parent(product) is saved in DB associated employees will also saved
- * REFRESH:when parent(product) is REFRESHED in DB associated employees will also refreshed
- * MERGE: when parent(eg productA is merged with other productsB),  then associated child of productA is also merged with productB.
- * DETACH: detach list of customers from its parent (product)
- * 
- * Avoid using ALL- because it includes all the above , in this project we dont want ot have REMOVE, ie when parent is deleted, its associated child 
- * should not be deleted.
- * 
- * LAZY:  Recommended- if parent is quered in DB its associated child is not loaded from DB, only parent is loaded from DB
- * EAGER: performance wise  it is down , if parent is queried in DB , its associated child is also loaded along with parent
- * 
- * Its recommenended to use cascade in Child side or (owning class) because many cutsomer are asociated with one product, if a product
- * is deleted then child has to react based upon the cascading rules.
- * */
