c3ebd77326044fec2edd10201820a015c0a3d510
Deepak <130002300+repo2606@users.noreply.github.com>
2023-05-05 18:06:10 -0400

    Update ProductController.java

--- src/main/java/com/deepak/sma/controller/ProductController.java
+++ src/main/java/com/deepak/sma/controller/ProductController.java
@@ -87,27 +87,4 @@ public class ProductController {
 
 
 
- /*Theory:
-  * 
-  * Model: use to exchange  data between view and controller (bind data between java code and html code , Model is used)
-  * h2- rutime, test db, default url: 
-jdbc:h2:mem:testdb
-
-
-theory: @PostMapping("/save")
-  * 
-  * 1.@RequestParam List<Long> customer,
-  *  as we pass customerId as part of url request , 
-  * Ids are numbers , depending upon no of customer we selected eg 3, there will be 3 customerid 
-  *   so we use List<long> to hold no of  3 ids and 
-  * we use "customer" as ref name (List<Long> customer) because query parameter also has same name
-  * so now  List<Long> customer will holds no of ids that were passed
-  * 
-  * 		Iterable<Customer>  chosenCustomer = custRepo.findAllById(customer);// based upon ID customer objects is retrieved in DB
-  * and those group of customer objects are iterated one by one
-  * 
-  * cust.setTheproducts(product); // for each customer object we set the products so that now association netween customer and product table is formed
-  * 
-  * 	custRepo.save(cust);// finally we save the customer which has product associated with it in database
-  * */
  
